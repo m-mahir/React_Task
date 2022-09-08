fcb489f18826b774687a8e24c4066dff667c843d
Mohamed Maher <mohammed.mahir92@gmail.com>
2022-09-08 10:58:29 +0200

    Initial commit

---
+++ src/Components/CountryList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CountryList() {
+  return (
+    <section>
+      <ul className="card country-list" data-testid="countryList">
+        <li className="pa-10 pl-20"></li>
+      </ul>
+    </section>
+  );
+}
+
+export default CountryList;
---
+++ src/Components/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Search() {
+  return (
+    <input
+      data-testid="filterInput"
+      className="large"
+      placeholder="Enter Country Name"
+    />
+  );
+}
+
+export default Search;
